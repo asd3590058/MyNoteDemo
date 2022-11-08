89fabb73add4c158851c1e14d3e4ecf0bdf73677
wuhongji-lhq <Zxc003986@>
2022-11-08 14:53:39 +0800

    update demo

--- app/src/main/java/com/example/activity/MainActivity.kt
+++ app/src/main/java/com/example/activity/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.activity
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mydemo.databinding.ActivityMainBinding
 
@@ -13,9 +12,10 @@ import com.example.mydemo.databinding.ActivityMainBinding
  *@description
  */
 class MainActivity : AppCompatActivity() {
-    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
     }
 }
